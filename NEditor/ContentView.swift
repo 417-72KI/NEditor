fe51b37066a679b3fc5970320cf138e77840894c
417-72KI <417.72ki@gmail.com>
2020-05-16 14:44:37 +0900

    Initial Commit

---
+++ NEditor/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  NEditor
+//
+//  Created by 417.72KI on 2020/05/16.
+//  Copyright © 2020 417.72KI. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
